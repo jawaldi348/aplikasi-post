46862d66ab5af971432b038fe1fbd5c24de931b2
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-12-16 23:57:23 +0700

    cek session ketika akses route welcome

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -1,25 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+class Welcome extends CI_Controller
+{
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		if ($this->session->userdata('masuk') == true) {
+			if ($this->session->userdata('idgrup') == '1') {
+				redirect('admin/home/index');
+			} else if ($this->session->userdata('idgrup') == '2') {
+				redirect('k/home/index');
+			} else {
+				redirect('login/logout');
+			}
+		} else {
+			redirect('login/logout');
+		}
 	}
 }
