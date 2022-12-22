5ce44531aae08df2ed8950a99bc388f6b7406c89
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-12-22 09:31:57 +0700

    update tabel dan nama field

--- app/models/Mcommon.php
+++ app/models/Mcommon.php
@@ -3,12 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Mcommon extends CI_Model
 {
-    public function get_toko()
+    public function get_bisnis()
     {
-        $sql = $this->db->where('id_toko', 1)->get('toko')->row_array();
+        $sql = $this->db->where('id_bisnis', 1)->get('bisnis')->row_array();
         $data = [
-            'toko' => $sql['nama_toko'],
-            'logo' => $sql['logo_toko'] == '' || !file_exists('app_content/' . $sql['logo_toko']) ? '' : base_url() . 'app_content/' . $sql['logo_toko']
+            'toko' => $sql['nama_bisnis'],
+            'logo' => $sql['logo_bisnis'] == '' || !file_exists('app_content/' . $sql['logo_bisnis']) ? '' : base_url() . 'app_content/' . $sql['logo_bisnis']
         ];
         return $data;
     }
