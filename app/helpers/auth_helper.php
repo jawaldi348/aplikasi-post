8e41434fdd5cbf1d67bd0ad91cc98f6fe375bd80
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2023-02-12 12:39:38 +0700

    function exit

--- app/helpers/auth_helper.php
+++ app/helpers/auth_helper.php
@@ -6,6 +6,7 @@ if (!function_exists('check_logged_in')) {
         $CI = &get_instance();
         if ($CI->session->userdata('masuk') != true) :
             redirect('logout');
+            exit;
         endif;
     }
 }
