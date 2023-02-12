fe6700d2e2e6296c2334653298f56a008ab7d28a
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2023-02-12 07:14:52 +0700

    tambah function helper check_logged_in

---
+++ app/helpers/auth_helper.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+if (!function_exists('check_logged_in')) {
+    function check_logged_in()
+    {
+        $CI = &get_instance();
+        if ($CI->session->userdata('masuk') != true) :
+            redirect('logout');
+        endif;
+    }
+}
