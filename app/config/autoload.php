7229fa7d92beff60961787f9a32dfda43e713ae9
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2023-02-27 10:19:35 +0700

    helper validasi

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'auth', 'common', 'layout');
+$autoload['helper'] = array('url', 'form', 'auth', 'common', 'layout', 'validasi');
 
 /*
 | -------------------------------------------------------------------
