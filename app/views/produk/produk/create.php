809ec170038da1656b3683dcd7c34fcb6a3f0572
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2023-03-03 08:47:00 +0700

    tambah inputan kode barcode dan nama produk

--- app/views/produk/produk/create.php
+++ app/views/produk/produk/create.php
@@ -1,3 +1,25 @@
 @extends(layouts/index)
 @section(content)
+<div class="col-lg-12">
+    <div class="card m-b-30">
+        <div class="card-header bg-default text-white">
+            <a href="<?= site_url('produk') ?>" class="btn btn-warning">&laquo; Kembali</a>
+        </div>
+        <div class="card-body">
+            <div class="alert alert-info">Silahkan Tambahkan Data Produk Melalui Form Berikut :</div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Kode Barcode/Produk<sup style="color: red;">*</sup></label>
+                <div class="col-sm-8">
+                    <input type="text" name="kode" class="form-control" autofocus="autofocus" autocomplete="off">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Nama Produk<sup style="color: red;">*</sup></label>
+                <div class="col-sm-10">
+                    <input type="text" name="nama" class="form-control" placeholder="Isikan Dengan Lengkap Nama Produk">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
